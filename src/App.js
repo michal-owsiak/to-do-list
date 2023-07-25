1d5b9409501ee596554fe7b517969c3999348bc1
Michal Owsiak <micowsiak@gmail.com>
2023-07-25 16:14:02 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -21,13 +21,20 @@ function App() {
   };
 
   const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map (task => {
+    setTasks(tasks => tasks.map(task => {
       if (task.id === id) {
         return { ...task, done: !task.done };
       }
 
       return task;
     }))
+  };
+
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true
+    })))
   };
 
   return (
@@ -44,6 +51,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
         mainSectionContent={
