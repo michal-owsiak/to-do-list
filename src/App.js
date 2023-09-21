e2284cb80d8612331edd71c319f8237b5d25c8dd
Michal Owsiak <micowsiak@gmail.com>
2023-09-21 20:09:10 +0200

    navigation & routes added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
+import Navigation from "./common/Navigation";
+import { Wrapper } from "./styled";
 
-export default () => <Tasks />;
+export default () => (
+  <HashRouter>
+    <Navigation />
+    <Wrapper>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania" />
+        </Route>
+      </Switch>
+    </Wrapper>
+  </HashRouter>
+);
