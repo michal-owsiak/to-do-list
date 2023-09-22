0a04afbae575051e51017b20466bbd1aed34b4aa
Michal Owsiak <micowsiak@gmail.com>
2023-09-22 11:37:57 +0200

    query custom hooks created

---
+++ src/features/tasks/TasksPage/queryParameters.js
@@ -0,0 +1,24 @@
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
+export const useQueryParameter = (paramName) => {
+    const location = useLocation();
+    return new URLSearchParams(location.search).get(paramName);
+};
+
+export const useReplaceQueryParameter = () => {
+    const history = useHistory();
+    const location = useLocation();
+
+    const replaceQueryParameter = ({ key, value }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (value === undefined) {
+            searchParams.delete(key);
+        } else {
+            searchParams.set(key, value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`)
+    };
+    return replaceQueryParameter;
+};
